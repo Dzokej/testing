2b8d3121a9c0c2750fce41b8cbac42410e41a9f2
Marc DeSantis <akoric9805@slps.org>
2016-03-03 14:59:14 -0600

    Something..
    Something described..

---
+++ reading.java
@@ -0,0 +1,23 @@
+/**
+ * @(#)reading.java
+ *
+ *
+ * @author 
+ * @version 1.00 2016/2/22
+ */
+
+public class reading {
+        
+    /**
+     * Creates a new instance of <code>reading</code>.
+     */
+    public reading() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
